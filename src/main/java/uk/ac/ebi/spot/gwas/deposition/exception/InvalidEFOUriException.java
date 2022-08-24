d4604e2425f86e3909698ad0e99720a521083169
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2022-08-24 13:52:33 +0100

    curation-138 Duplicate Efo Traits Issue

---
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/exception/InvalidEFOUriException.java
@@ -0,0 +1,8 @@
+package uk.ac.ebi.spot.gwas.deposition.exception;
+
+public class InvalidEFOUriException extends RuntimeException {
+
+    public InvalidEFOUriException(String message) {
+        super(message);
+    }
+}

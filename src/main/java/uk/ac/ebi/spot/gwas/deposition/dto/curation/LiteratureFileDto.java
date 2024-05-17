6cab936761b528a3745ed61d3d0b586e770a0cc5
sprintell <abayomi@sprintell.org>
2024-05-17 09:20:31 +0100

    Extend Literature DTO

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/dto/curation/LiteratureFileDto.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/dto/curation/LiteratureFileDto.java
@@ -3,9 +3,11 @@ package uk.ac.ebi.spot.gwas.deposition.dto.curation;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springframework.hateoas.core.Relation;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.AssertTrue;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -14,11 +16,17 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Relation(
+        value = "literature-file",
+        collectionRelation = "literature-files"
+)
 public class LiteratureFileDto {
 
     private String id;
-
     private String fileName;
+    private String name;
+    private String createdBy;
+    private Date createDate;
 
     @JsonIgnore
     private List<MultipartFile> multipartFile;

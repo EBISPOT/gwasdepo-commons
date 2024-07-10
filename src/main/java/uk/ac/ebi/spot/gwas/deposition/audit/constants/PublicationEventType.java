4b4e19385d838c93c61df169ad95423f5e1442d8
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2024-07-10 10:36:31 +0100

    goci-1313-curation-tracking-feature Adding Long desc events

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/audit/constants/PublicationEventType.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/audit/constants/PublicationEventType.java
@@ -3,25 +3,22 @@ package uk.ac.ebi.spot.gwas.deposition.audit.constants;
 public enum PublicationEventType {
 
 
-     SUBMISSION_CREATED("Submission created"),
-    TEMPLATE_UPLOAD("Template uploaded"),
-    CURATION_STATUS_UPDATED("Curation status updated"),
-    CURATOR_UPDATED("Curator updated"),
-    PMID_CREATED("Publication created in database"),
-    PMID_ADDED("PMID added to submission"),
-    SUBMISSION_UPDATE("Submission updated"),
-    TRAIT_UPDATED("Trait updated"),
-    SAMPLE_UPDATED("Sample updated"),
-    SNP_APPROVED("Associations approved"),
-    IMPORTED("Imported to curation database"),
-    MAPPING_COMPLETE("Mapping complete"),
-    SS_VALIDATION_FAILED("Failed validation of sumstats"),
-    SS_VALIDATION_SUCCESS("Successful validation of sumstats"),
-    METADATA_VALIDATION_FAILED("Failed validation of metadata"),
-    METADATA_VALIDATION_SUCCESS("Successful validation of metadata");
+    SUBMISSION_CREATED,
+    TEMPLATE_UPLOAD,
+    CURATION_STATUS_UPDATED,
+    CURATOR_UPDATED,
+    PMID_CREATED,
+    PMID_ADDED,
+    SUBMISSION_UPDATE,
+    TRAIT_UPDATED,
+    SAMPLE_UPDATED,
+    SNP_APPROVED,
+    IMPORTED,
+    MAPPING_COMPLETE,
+    SS_VALIDATION_FAILED,
+    SS_VALIDATION_SUCCESS,
+    METADATA_VALIDATION_FAILED,
+    METADATA_VALIDATION_SUCCESS;
 
-    private String value = null;
-    PublicationEventType(String value) {
-        this.value = value;
-    }
+
 }
